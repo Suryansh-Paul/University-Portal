ccf2bce54ca7a0426c386141511acd8b7933d387
Suryansh-Paul <suryanshpaul65@gmail.com>
2026-07-10 01:07:56 +0530

    feat: return JWT on successful login

--- backend/src/main/java/com/evan/uni/controller/authcontroller.java
+++ backend/src/main/java/com/evan/uni/controller/authcontroller.java
@@ -1,6 +1,8 @@
 package com.evan.uni.controller;
 
+import com.evan.uni.dtos.LoginResponse;
 import com.evan.uni.model.User;
+import com.evan.uni.security.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class authcontroller {
     @Autowired
     private AuthenticationManager authenticationManager;
-
+    @Autowired
+    private JwtService service;
     @PostMapping("/login")
-    ResponseEntity<Authentication> login(@RequestBody User user) {
+    ResponseEntity<LoginResponse> login(@RequestBody User user) {
 
 
 
 
         Authentication AuthenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()) ;
-    Authentication authentication =  authenticationManager.authenticate(AuthenticationToken);
-          return new ResponseEntity<>(authentication, HttpStatus.OK);
+        authenticationManager.authenticate(AuthenticationToken);
+    String jwt = service.generateToken(user.getUsername());
+          return  ResponseEntity.ok( new LoginResponse(jwt));
     }
 
 

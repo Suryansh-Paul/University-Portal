e7047614617055473ad460c1635adb62507e2e0f
Suryansh-Paul <suryanshpaul65@gmail.com>
2026-07-09 17:01:04 +0530

    feat: configure Spring Security authentication

---
+++ backend/src/main/java/com/evan/uni/security/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.evan.uni.security;
+
+import com.evan.uni.service.MyUserDetail_service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+     @Autowired
+    private MyUserDetail_service myUserDetailService ;
+
+
+
+     @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request-> request
+                .requestMatchers("/login")
+                .permitAll()
+                .anyRequest().authenticated());
+        http.csrf(csrf -> csrf.disable());
+           return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+     public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider() ;
+       provider.setUserDetailsService(myUserDetailService);
+       provider.setPasswordEncoder(passwordEncoder());
+                 return provider;
+    }
+    @Bean
+   public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+           return config.getAuthenticationManager();
+    }
+
+
+
+
+
+
+
+}
